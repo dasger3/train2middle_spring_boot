628ac3804fb67664227dfc93ecd9754089734220
Oleksii_Voronin2 <oleksii_voronin2@epam.com>
2022-02-01 18:47:27 +0200

    Change some configuration

--- src/main/java/com/train2middle/springboot/SpringbootApplication.java
+++ src/main/java/com/train2middle/springboot/SpringbootApplication.java
@@ -1,13 +1,30 @@
 package com.train2middle.springboot;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootApplication {
+public class SpringbootApplication extends SpringBootServletInitializer implements CommandLineRunner {
+
+	private static final Logger logger = LoggerFactory.getLogger(SpringbootApplication.class);
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringbootApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) {
+		logger.info("Hello world");
 	}
 
 }
